d8e524d9c16eae3be218b36b5c9ead272d7b577b
Antti Kurkinen <antti.kurkinen@struu.com>
2019-11-16 16:11:59 +0200

    fuse search

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-const bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const { ProductSchema }  = require('../models/product');
+const { ProductSchema } = require('../models/product');
+const Fuse = require('fuse.js');
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
@@ -11,9 +12,16 @@ router.get('/', async (req, res, next) => {
 
 router.post('/search', async (req, res, next) => {
   const { query } = req.body;
-  const products = await ProductSchema.find({ $text: { $search: query } }).limit(10).exec();
+  //const products = await ProductSchema.find({ $text: { $search: query } }).limit(10).exec();
+  const products = await ProductSchema.find().lean().exec()
+  
+  const options = {
+    keys: ['name']
+  }
+  const fuse = new Fuse(products, options);
+  const results = fuse.search(query);
 
-  return res.json(products);
+  return res.json(results);
 })
 
 module.exports = router;
