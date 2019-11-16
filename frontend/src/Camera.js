f8a4d816b7e9a04fb25edbf13418d4b9742c234c
Antti Kurkinen <antti.kurkinen@struu.com>
2019-11-16 21:03:20 +0200

    Add stuff

--- frontend/src/Camera.js
+++ frontend/src/Camera.js
@@ -8,35 +8,50 @@ import Fuse from 'fuse.js';
 
 import data from './data/verkkokauppa.json';
 
+import { getBudget } from './Home';
+
 const fuse = new Fuse(data, {
   keys: ['name']
 })
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
+const timespanOptions = [
+  { value: 0.25, label: '1 Week' },
+  { value: 0.5, label: '2 Weeks' },
+  { value: 1, label: '1 Month' },
+  { value: 2, label: '2 Months' },
+  { value: 3, label: '3 Months' },
+  { value: 4, label: '4 Months' },
+  { value: 5, label: '5 Months' },
+  { value: 6, label: '6 Months' },
+  { value: 12, label: '1 Year' },
+  { value: 24, label: '2 Years' },
+  { value: 36, label: '3 Years' },
+  { value: 48, label: '4 Years' },
+  { value: 60, label: '5 Years' },
+  { value: 120, label: '10 Years' },
 ];
 
-function calculateMonthlyPrice(price, lifetime){
-  return Math.ceil(price / lifetime)
+function calculateMonthlyPrice(price, { value }) {
+  console.log(price, value)
+  return Math.ceil(price / value)
 }
 
 function parsePrice(text){
   try {
-    const match = text.match(/\d+[\.|\,]\d{1,2}/gm);
-    return match[0]
+    const match = text.replace(/ /g, '').match(/\d+[\.|\,]\d{1,2}/gm);
+    return match[0].replace(/,/g, '.');
   } catch(e) {
     console.log(e);
-    return '';
+    return 0;
   }
 }
 
 function CameraWrapper(props) {
   const [text, setText] = useState('');
   const [price, setPrice] = useState('');
-  const [timespan, setTimespan] = useState('chocolate');
+  const [timespan, setTimespan] = useState(timespanOptions[2]);
   const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState('');
 
   const recognize = (dataUrl) => {
     Tesseract.recognize(
@@ -46,12 +61,14 @@ function CameraWrapper(props) {
     ).then((data) => {
       console.log(data);
       const { text } = data;
-      const results = fuse.search(text).map(e => ({
+      const searchTerm = text.substring(0, 20);
+      const results = fuse.search(searchTerm).map(e => ({
         value: e.name, 
         label: e.name,
       }));
       console.log(results);
       setProducts(results);
+      setProduct(products[0]);
       setPrice(parsePrice(text));
       setText(text);
     })
@@ -60,13 +77,9 @@ function CameraWrapper(props) {
   const submit = (e) => {
     e.preventDefault();
     console.log(e);
-    const searchTerm = text.substring(0, 20);
-    const results = fuse.search(searchTerm).map(e => ({
-      value: e.name, 
-      label: e.name,
-    }));
-    console.log(results);
   }
+
+  const estimatedCost = calculateMonthlyPrice(price, timespan);
 
   return (
     <div>
@@ -77,14 +90,18 @@ function CameraWrapper(props) {
       />
       <form onSubmit={submit}>
         <div>
-          <label>Name: </label>
-          <input 
-            type="text" 
-            value={text} 
-            onChange={(e) => setText(e.target.value)}
+          {/* <label>Product: </label> */}
+          <Select 
+            className="selector"
+            value={product} 
+            onChange={(value) => setProduct(value)}
+            options={products}
+            isSearchable={true}
+            placeholder="Product name"
           />
         </div>
         <div>
+          {/* <label>Price: </label> */}
           <input 
             type="text" 
             value={price} 
@@ -93,15 +110,17 @@ function CameraWrapper(props) {
           />
         </div>
         <div>
+          {/* <label>Lifespan: </label> */}
           <Select 
             className="selector"
-            value={products} 
-            onChange={(value) => setProducts(value)}
-            options={products}
+            value={timespan} 
+            onChange={(value) => setTimespan(value)}
+            options={timespanOptions}
           />
         </div>
-        <input type="submit" value="Search" />
+        {/* <input type="submit" value="Search" /> */}
       </form>
+      <h2>Estimated cost: {estimatedCost}€/month</h2>
     </div>
   )
 }
